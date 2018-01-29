4e44c347637ab6d35e8b5e3f48969ada4cde0d62
sravanthi velaga <svelaga0412@gmail.com>
2018-01-28 22:54:37 -0800

    Add Controllers for the BlogPost

---
+++ MyPortfolio/Controllers/BlogPostController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyPortfolio.Controllers
+{
+    [Authorize]
+    public class BlogPostController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public BlogPostController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            return View(_db.BlogPosts.Where(x => x.User.Id == currentUser.Id));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(BlogPost blogpost)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            blogpost.User = currentUser;
+            _db.BlogPosts.Add(blogpost);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Posts()
+        {
+            return Json(_db.BlogPosts.ToList());
+        }
+    }
+}
